cca8c8f486ffdeed399aa2aebcff587bd65efe48
Eric Schmidt <easchmidtjr@gmail.com>
2016-03-02 15:38:43 -0500

    Refactor movement code

--- src/main.js
+++ src/main.js
@@ -7,11 +7,14 @@
 
 (function() {
 
+	// CONSTANTS
+	var MOVE_SPEED = 150;
+
 	// GAME VARIABLES
-	var attack;
 	var player;
 	var bacteria;
 	var cursors;
+	var keys;
 	var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 	// HELPER FUNCTIONS
@@ -21,6 +24,11 @@
 		// Removes the star from the screen
 		star.kill();
 
+	}
+
+	// Loads and displays a level to play
+	function loadLevel(num) {
+		// stuff
 	}
 
 	// MAIN PHASER FUNCTIONS
@@ -38,47 +46,32 @@
 
 	// Initialize game
 	function create() {
-
-		//  We're going to be using physics, so enable the Arcade Physics system
+		// Enable the Arcade Physics system
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
 		//  A simple background for our game
 		//game.add.sprite(0, 0, 'sky');
 
-		//  The platforms group contains the ground and the 2 ledges we can jump on
-		bacteria = game.add.group();
-
-		//  We will enable physics for any object that is created in this group
-		bacteria.enableBody = true;
-
-		// Here we create the ground.
-		var group1 = bacteria.create(20, 20, 'bacteria');
-
-		//  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-		//ground.scale.setTo(2, 2);
-
-		//  This stops it from falling away when you jump on it
-		group1.body.immovable = true;
-
+		// Create a group for the bacteria
+		bacteria = game.add.physicsGroup();
 
 		// The player and its settings
 		player = game.add.sprite(32, game.world.height - 150, 'player');
-
-		//  We need to enable physics on the player
 		game.physics.arcade.enable(player);
 
-		//  Player physics properties. Give the little guy a slight bounce.
-		//player.body.bounce.y = 0.2;
-		//player.body.gravity.y = 300;
+		// Player physics properties. Give the little guy a slight bounce
 		player.body.collideWorldBounds = true;
 
-		//  Our two animations, walking left and right.
-		//player.animations.add('left', [0, 1, 2, 3], 10, true);
-		//player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-		//  Our controls.
+		// Keyboard controls
 		cursors = game.input.keyboard.createCursorKeys();
-		
+		console.log(game.input.keyboard);
+		keys = game.input.keyboard.addKeys({
+			'w': Phaser.Keyboard.W,
+			'a': Phaser.Keyboard.A,
+			's': Phaser.Keyboard.S,
+			'd': Phaser.Keyboard.D,
+			'attack': Phaser.Keyboard.SPACEBAR
+		});
 	}
 
 	// The main game loop
@@ -87,39 +80,26 @@
 		//  Collide the player and the stars with the platforms
 		game.physics.arcade.collide(player, bacteria);
 
-		//  Reset the players velocity (movement)
+		//  Reset the player's velocity (movement)
 		player.body.velocity.x = 0;
 		player.body.velocity.y = 0;
 
-		if (cursors.left.isDown)
-		{
-			//  Move to the left
-			player.body.velocity.x = -150;
-
+		if (cursors.left.isDown || keys.a.isDown) {
+			// Move to the left
+			player.body.velocity.x = -MOVE_SPEED;
 		}
-		if (cursors.right.isDown)
-		{
-			//  Move to the right
-			player.body.velocity.x = 150;
-
+		if (cursors.right.isDown || keys.d.isDown) {
+			// Move to the right
+			player.body.velocity.x = MOVE_SPEED;
 		}
-		if (cursors.up.isDown)
-		{
+		if (cursors.up.isDown || keys.w.isDown) {
 			// Move up
-
-			player.body.velocity.y = -150;
+			player.body.velocity.y = -MOVE_SPEED;
 		}
-		if (cursors.down.isDown)
-		{
+		if (cursors.down.isDown || keys.s.isDown) {
 			// Move down
-
-			player.body.velocity.y = 150;
+			player.body.velocity.y = MOVE_SPEED;
 		}
-		else
-		{
-			player.body.velocity
-		}
-
 	}
 
 })();
