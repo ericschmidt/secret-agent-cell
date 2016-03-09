93d47507eb12d717d824b3ce78c974f948298525
Eric Schmidt <easchmidtjr@gmail.com>
2016-03-09 16:41:44 -0500

    Multiple strains of bacteria

--- src/game.js
+++ src/game.js
@@ -8,30 +8,31 @@
 (function() {
 
 	// Game constants
-	var MOVE_SPEED = 180;
 	var ATTACK_RADIUS_SQUARED = 60*60;
+	var ATTACK_TIME = 40; // Attack period, lower value - faster attacking
 	var BULLET_DAMAGE = 40;
 	var BULLET_SPEED = 200;
 	var GRID_SIZE = 40;
 	var GROWTH_TIME = 200; // Growth period, lower value - faster bac growth
+	var MAX_Y = 510;
+	var MOVE_SPEED = 180;
 	var PLAYER_MAX_HEALTH = 100;
-	var REGEN_TIME = 50; // Regen period, lower value - faster regen
 	var REGEN_AMOUNT = 5; // Regen amount, how much you regen per tick
+	var REGEN_TIME = 50; // Regen period, lower value - faster regen
 	var SHOOT_TIME = 180; // Shooting period, lower value - faster shooting
-	var ATTACK_TIME = 40; // Attack period, lower value - faster attacking
 	var SPAWN_RATE = 0.3;
 
 	// Game variables
 	var player;
 
-	var bacteriaGrid;		// A 2D array to store bacteria positions
-	var bacteria;			// The group of bacteria
-	var enemyBullets;		// The group of enemy bullets
-	var firingCounter = 0;	// Counter determining when bacteria can fire
-	var growthCounter = 0;	// Counter to determine when bacteria grow
-	var regenCounter = 0;	// Counter for regen
-	var shrinkCounter = 0;  // Counter for re-shrinking
-	var attackCounter = 40;  // Counter for attacking
+	var bacteriaGrid;			// A 2D array to store bacteria positions
+	var bacteriaGroups = [];	// The list of bacteria groups
+	var enemyBullets;			// The group of enemy bullets
+	var firingCounter = 0;		// Counter determining when bacteria can fire
+	var growthCounter = 0;		// Counter to determine when bacteria grow
+	var regenCounter = 0;		// Counter for regen
+	var shrinkCounter = 0;		// Counter for re-shrinking
+	var attackCounter = 40;		// Counter for attacking
 
 	var cursors;
 	var keys;
@@ -49,7 +50,9 @@
 			// Load all the needed resources for the menu.
 			GameInstance.load.image('background', './assets/background.png');
 			GameInstance.load.spritesheet('player', 'assets/agentcell.png', 33, 32);
-			GameInstance.load.spritesheet('bacteria', 'assets/bac1.png', 33, 33);
+			GameInstance.load.spritesheet('bacteria1', 'assets/bac1.png', 33, 33);
+			GameInstance.load.spritesheet('bacteria2', 'assets/bac2.png', 40, 40);
+			GameInstance.load.spritesheet('bacteria3', 'assets/bac3.png', 33, 33);
 			GameInstance.load.spritesheet('kaboom', 'assets/explode.png', 128, 128);
 			GameInstance.load.image('enemyBullet', 'assets/bullet7.png');
 			GameInstance.load.image('enemyBullet2', 'assets/bullet1.png');
@@ -82,17 +85,17 @@
 			enemyBullets.setAll('outOfBoundsKill', true);
 			enemyBullets.setAll('checkWorldBounds', true);
 
-			// Create a group for the bacteria
-			bacteria = GameInstance.add.group();
-			bacteria.enableBody = true;
-			bacteria.physicsBodyType = Phaser.Physics.ARCADE;
+			// Create 3 groups for the bacteria strains
+			bacteriaGroups.push(GameInstance.add.physicsGroup());
+			bacteriaGroups.push(GameInstance.add.physicsGroup());
+			bacteriaGroups.push(GameInstance.add.physicsGroup());
 			
 			// Initialize health
 			health = PLAYER_MAX_HEALTH;
 			healthBorder = GameInstance.add.sprite(15, GameInstance.world.height - 15 , 'health_border');
 			healthbar = GameInstance.add.sprite(15 + 3, GameInstance.world.height - 15 - 3, 'health_red');
-			healthbar.anchor.setTo(0,1);
-			healthBorder.anchor.setTo(0,1);
+			healthbar.anchor.setTo(0, 1);
+			healthBorder.anchor.setTo(0, 1);
 
 			// State text - invisible
 			stateText = GameInstance.add.text(GameInstance.world.centerX, GameInstance.world.centerY, ' ', { font: '84px Arial', fill: '#fff' });
@@ -101,7 +104,6 @@
 
 			// Audio
 			bloop = GameInstance.add.audio('bloop');
-
 
 			//Adding menu button
 			menuButton = Game.add.button(WIDTH , HEIGHT, 'menuButton', Game.startMenu, Game);
@@ -118,38 +120,37 @@
 				'attack': Phaser.Keyboard.SPACEBAR
 			});
 
-			
-
 			Game.loadLevel(0);
 
 		},
 
 		update: function() {
-
-			GameInstance.physics.arcade.collide(player, bacteria);
-
 			// Handle bacteria growth
 			growthCounter++;
 			if (growthCounter > GROWTH_TIME) {
-				Game.growBacteria();
+				Game.growBacteria(0);
+				Game.growBacteria(1);
+				Game.growBacteria(2);
 				growthCounter = 0;
 			}
 		
 			// Handle firing counters
-			bacteria.forEach(function(d){
-				d.counter++;
-				if (d.counter === SHOOT_TIME-100){
-					d.animations.add('shooting');
-					d.animations.play('shooting', 6, false);
-				}
-				if (d.counter === SHOOT_TIME){
-					d.counter = 0;
-					d.frame = 0;
-					//Game.fourWay(d);
-					Game.playerChaser(d);
-					bloop.play();
-				}
-			});
+			for (var i=0; i < bacteriaGroups.length; i++) {
+				bacteriaGroups[i].forEach(function(d){
+					d.counter++;
+					if (d.counter === SHOOT_TIME-100){
+						d.animations.add('shooting');
+						d.animations.play('shooting', 6, false);
+					}
+					if (d.counter === SHOOT_TIME){
+						d.counter = 0;
+						d.frame = 0;
+						//Game.fourWay(d);
+						Game.playerChaser(d);
+						bloop.play();
+					}
+				});
+			}
 
 			// Healthbar animator
 			if (health < PLAYER_MAX_HEALTH) {
@@ -177,7 +178,9 @@
 			attackCounter++;
 
 			// Physics checkers
-			GameInstance.physics.arcade.collide(player, bacteria);
+			for(var i=0; i < bacteriaGroups.length; i++) {
+				GameInstance.physics.arcade.collide(player, bacteriaGroups[i]);
+			}
 			GameInstance.physics.arcade.overlap(enemyBullets, player, Game.enemyHitsPlayer, null, Game);
 
 			//  Reset the player's velocity
@@ -206,9 +209,6 @@
 				player.animations.play('attack');
 			}
 
-			//checks you win the level
-			if (bacteria.countLiving()===0)
-				console.log("WIN");
 		},
 
 		loadLevel: function(num) {
@@ -223,18 +223,18 @@
 				player.x = level.playerStart.x * GRID_SIZE;
 				player.y = level.playerStart.y * GRID_SIZE;
 				// Spawn the initial bacteria
-				bacteria.removeAll(true);
+				Game.removeAllBacteria();
 				Game.clearBacteriaGrid();
-				for (var i=0; i < level.bacteriaStarts.length; i++) {
-					var pos = level.bacteriaStarts[i];
-					Game.spawnBacteria(pos.x, pos.y);
+				for (var i=0; i < level.bacteria.length; i++) {
+					var bac = level.bacteria[i];
+					Game.spawnBacteria(bac.strain, bac.x, bac.y);
 				}
 
 
 			}
 		},
 
-		growBacteria: function() {
+		growBacteria: function(strainNum) {
 			var neighborhood = [
 				{x: 1, y: 0},
 				{x: -1, y: 0},
@@ -243,14 +243,14 @@
 			];
 			var perimeter = [];
 			// Get the grid positions along the perimeter of the current bacteria
-			for (var i=0; i < bacteria.length; i++) {
-				var bac = bacteria.getAt(i);
+			for (var i=0; i < bacteriaGroups[strainNum].length; i++) {
+				var bac = bacteriaGroups[strainNum].getAt(i);
 				var gridX = Math.floor(bac.x / GRID_SIZE);
 				var gridY = Math.floor(bac.y / GRID_SIZE);
 				for (var j=0; j < neighborhood.length; j++) {
 					var neighborX = gridX + neighborhood[j].x;
 					var neighborY = gridY + neighborhood[j].y;
-					if (Game.inGridBounds(neighborX, neighborY) && !Game.bacteriaAt(neighborX, neighborY)
+					if (Game.inGameBounds(neighborX, neighborY) && !Game.bacteriaAt(neighborX, neighborY)
 						&& !Game.containsPosition(perimeter, neighborX, neighborY)) {
 						perimeter.push({x: neighborX, y: neighborY});
 					}
@@ -259,8 +259,14 @@
 			// Spawn bacteria along the perimeter
 			for (var i=0; i < perimeter.length; i++) {
 				if (Math.random() < SPAWN_RATE) {
-					Game.spawnBacteria(perimeter[i].x, perimeter[i].y);
+					Game.spawnBacteria(strainNum, perimeter[i].x, perimeter[i].y);
 				}
+			}
+		},
+
+		removeAllBacteria: function() {
+			for (var i=0; i < bacteriaGroups.length; i++) {
+				bacteriaGroups[i].removeAll(true);
 			}
 		},
 
@@ -278,10 +284,10 @@
 			return !!bacteriaGrid[gridX][gridY];
 		},
 
-		// Checks if a 2D position is within grid bounds
-		inGridBounds: function(x, y) {
-			return (x >= 0 && x < Math.floor(WIDTH/GRID_SIZE)
-					&& y >= 0 && y < Math.floor(HEIGHT/GRID_SIZE));
+		// Checks if a 2D position is within game bounds
+		inGameBounds: function(x, y) {
+			return (x > 0 && x < Math.floor(WIDTH/GRID_SIZE)
+					&& y > 0 && y < Math.floor(MAX_Y/GRID_SIZE)+1);
 		},
 
 		// Clears the bacteria grid
@@ -297,18 +303,38 @@
 		},
 
 		// Spawns a bacteria in a given grid position
-		spawnBacteria: function(x, y) {
+		spawnBacteria: function(strainNum, x, y) {
+			if (strainNum < 0 || strainNum > 2) {
+				// Not a valid strain, do nothing
+				return;
+			}
 			var playerGridX = Math.floor(player.x / GRID_SIZE);
 			var playerGridY = Math.floor(player.y / GRID_SIZE);
 			// Don't spawn if player is there
 			if (playerGridX === x && playerGridY === y) {
 				return;
 			}
-			var newBacteria = bacteria.create(x*GRID_SIZE, y*GRID_SIZE, 'bacteria');
+			var spriteName;
+			if (strainNum === 0) {
+				spriteName = 'bacteria1';
+			} else if (strainNum === 1) {
+				spriteName = 'bacteria2';
+			} else if (strainNum === 2) {
+				spriteName = 'bacteria3';
+			}
+			var newBacteria = bacteriaGroups[strainNum].create(x*GRID_SIZE, y*GRID_SIZE, spriteName);
 			newBacteria.anchor.setTo(0.5, 0.5);
 			newBacteria.body.immovable = true;
 			newBacteria.counter = 0;
 			bacteriaGrid[x][y] = true;
+		},
+
+		// Kill a bacteria
+		killBacteria: function(bac) {
+			var gridX = Math.floor(bac.x / GRID_SIZE);
+			var gridY = Math.floor(bac.y / GRID_SIZE);
+			bacteriaGrid[gridX][gridY] = false;
+			bac.destroy();
 		},
 
 		// Makes the player attack
@@ -316,13 +342,15 @@
 			player.scale.setTo(1.5, 1.5);
 			shrinkCounter = 0;
 			attackCounter = 0;
-			bacteria.forEach(function(bac) {
-				var dx = bac.x - player.x;
-				var dy = bac.y - player.y;
-				if (dx*dx + dy*dy < ATTACK_RADIUS_SQUARED) {
-					bac.destroy();
-				}
-			});
+			for (var i=0; i < bacteriaGroups.length; i++) {
+				bacteriaGroups[i].forEach(function(bac) {
+					var dx = bac.x - player.x;
+					var dy = bac.y - player.y;
+					if (dx*dx + dy*dy < ATTACK_RADIUS_SQUARED) {
+						Game.killBacteria(bac);
+					}
+				});
+			}
 		},
 
 		// The diagonal cross shooting pattern
